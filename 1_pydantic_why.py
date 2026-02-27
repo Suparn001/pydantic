39a22c324cd145268b09d5ca415f31402e2ac561
suparn <suparngautam@zapbuild.com>
2026-02-27 09:24:09 +0530

    leanerd about annotated filed vaidator where usercan add comments also or describe also for validation

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,15 +1,15 @@
 from pydantic import BaseModel,EmailStr,AnyUrl, Field
-from typing import List,Dict, Optional
+from typing import List,Dict, Optional, Annotated
 
 # step 1 create model
 class Patient(BaseModel):
-    name:str =  Field(max_length=50)
+    name:Annotated[str ,Field(max_length=50,title='Name of Subscriber',description='Give the name of patient in less then 50 chars',examples=['Nitish','Ajay'])]
     email:EmailStr
     linkedin_url:AnyUrl
     age:int
-    weight:float= Field(gt=0,le=60)
-    married:bool =False #this is default value
-    allergies:Optional[List[str]] =None # List[str] this is used to check iin list every value is string, but if we use list, ths will validate like, list is there, but does not ensure, itcotains string of not but List[str]does that
+    weight: Annotated[float,Field(gt=0,le=60, strict=True)] # strict = True mean, type coercin wont bethere, weight has o be float number, it cannot be string      
+    married:Annotated[bool, Field(default=False,description='Is the patient married or not')]
+    allergies:Annotated[Optional[List[str]], Field(default=None, description="allergies are optional", max_length=5)]  # List[str] this is used to check iin list every value is string, but if we use list, ths will validate like, list is there, but does not ensure, itcotains string of not but List[str]does that
     contact_details:Dict[str,str]
 
 
@@ -17,8 +17,6 @@ def insert_patients_data(patient: Patient):
     print(patient)
     print(patient.age)
     print(patient.name)
-
-
 
 patient_info = {
         'name':'nitishuuuuuuuuuuuuuuuuuu',
